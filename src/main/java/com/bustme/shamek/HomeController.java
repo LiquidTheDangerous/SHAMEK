e9d9ababe5a5960276f3f5917b9fd76d069f7073
ceckinkonstantin@gmail.com <ceckinkonstantin@gmail.com>
2022-12-18 02:13:20 +0500

    add authentication

--- src/main/java/com/bustme/shamek/HomeController.java
+++ src/main/java/com/bustme/shamek/HomeController.java
@@ -1,13 +1,20 @@
 package com.bustme.shamek;
 
+import org.springframework.context.annotation.Role;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("home")
+@RequestMapping("/")
 public class HomeController {
-    @GetMapping
+    @GetMapping()
+    public String main() {
+        return "home";
+    }
+
+    @GetMapping("/home")
     public String home() {
         return "home";
     }
