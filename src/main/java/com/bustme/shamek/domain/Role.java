79eed67de664822cf43e0b40bbe84280284f6df1
Andrey <semenov.andrey.2003@gmail.com>
2022-12-18 01:35:27 +0500

    Add roles/CRUD

---
+++ src/main/java/com/bustme/shamek/domain/Role.java
@@ -0,0 +1,5 @@
+package com.bustme.shamek.domain;
+
+public enum Role {
+    USER
+}
---
+++ src/main/java/com/bustme/shamek/repo/UserRepo.java
@@ -0,0 +1,7 @@
+package com.bustme.shamek.repo;
+import com.bustme.shamek.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepo extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
