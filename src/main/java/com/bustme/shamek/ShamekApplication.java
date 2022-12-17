7e78981f48e6dafb6d4adae7607a2b6fa5038c1b
Andrey <semenov.andrey.2003@gmail.com>
2022-12-18 00:03:31 +0500

    Initial commit

---
+++ src/main/java/com/bustme/shamek/ShamekApplication.java
@@ -0,0 +1,13 @@
+package com.bustme.shamek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShamekApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShamekApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bustme/shamek/ShamekApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bustme.shamek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShamekApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
