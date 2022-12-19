39f8eac76df65a572fecf11d3d8a028939c12ae8
Andrey <semenov.andrey.2003@gmail.com>
2022-12-19 22:31:27 +0500

    secure part

--- src/main/java/com/bustme/shamek/config/WebSecurityConfig.java
+++ src/main/java/com/bustme/shamek/config/WebSecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -49,4 +50,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 auth.inMemoryAuthentication()
                 .withUser("ADMIN").password("ADMIN").roles("ADMIN");
     }
+
+    @Override
+      public void configure(WebSecurity web) {
+        web.ignoring()
+            .antMatchers(
+                "/css/", "/fonts/",
+                "/images/**");
+      }
 }
